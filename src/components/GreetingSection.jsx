22b0a80901fa1fb63e904ec6e57e337a47a5e015
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2024-03-13 21:59:55 -0300

    added header

--- src/components/GreetingSection.jsx
+++ src/components/GreetingSection.jsx
@@ -14,13 +14,11 @@ const HeroSection = () => {
           <br></br>
           <TypeAnimation
             sequence={[
-              "Judy",
+              "Artur R. Alfa",
               1000,
-              "Web Developer",
+              "Full-Stack Developer",
               1000,
-              "Mobile Designer",
-              1000,
-              "UI/UX Designer",
+              "Mobile Developer",
               1000,
             ]}
             wrapper="span"
@@ -29,8 +27,7 @@ const HeroSection = () => {
           />
         </h1>
         <p className="text-[#ADB7BE] mb-6 textl-lg lg:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          voluptatum.
+          Welcome to my portfolio
         </p>
         <div>
           <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
@@ -44,13 +41,13 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-        <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
+        <div className="lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full overflow-hidden relative">
           <Image
             src="/images/photo.jpg"
             alt="photo"
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            width={300}
-            height={300}
+            className="absolute inset-0 w-full h-full object-cover"
+            layout="fill"
+            objectFit="cover"
           />
         </div>
       </div>
