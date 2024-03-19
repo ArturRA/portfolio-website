1a3e5ae454910cf3bdb7a20f9c0b7053fdf0fe8d
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2024-03-18 22:07:49 -0300

    fix links

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/ArturRA.com">
+          <Link href="https://github.com/ArturRA">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="https://www.linkedin.com/in/arturalfa/.com">
+          <Link href="https://www.linkedin.com/in/arturalfa/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
